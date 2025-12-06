323aba5d245908f3b3fc7edd8fa684a3b7d41c6e
Zyganadel <121130526+Zyganadel@users.noreply.github.com>
2025-12-06 16:34:09 -0400

    Add mask pickup class

---
+++ src/Gameplay/Objects/MaskPickup.gd
@@ -0,0 +1,13 @@
+class_name MaskPickup
+extends Node
+
+@export var MaskTime:int=900; # at 60tps this is 15s.
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
