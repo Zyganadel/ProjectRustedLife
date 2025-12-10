1003a86b11c9a19851ee6ddb49b94fa9621dba3c
WierdPotato <savahnagames@hotmail.com>
2025-12-09 21:21:43 -0600

    Buttonadd

---
+++ Scenes/Testing/LettersStuff.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+	
+func Onstartgame_buttonbutton_down() -> void:
+	print("level selection")
+	
+
+func Onsettings_buttonbutton_down() -> void:
+	print("settings")
+	
+
+func Onexit_buttonbutton_down() -> void:
+	print("exit")
+	
