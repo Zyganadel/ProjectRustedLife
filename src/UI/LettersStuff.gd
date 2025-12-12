f3e52471706ba9928489b62033d3a8325c81930f
WierdPotato <savahnagames@hotmail.com>
2025-12-11 23:11:07 -0600

    Menu and settings partially ready
    Settings Fullscreen is quite glitchy, it doesn't save the fullscreen mode, instead in turns off everytime. It keeps you in fs until you reenter settings again.

--- src/UI/LettersStuff.gd
+++ src/UI/LettersStuff.gd
@@ -9,15 +9,14 @@ func _physics_process(delta: float) -> void:
 	
 func Onstartgame_buttonbutton_down() -> void:
 	get_tree().change_scene_to_file("uid://ii6l6x1ifhuy")
-	print("level selection")
+	
 	
 
 func Onsettings_buttonbutton_down() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Testing/SettingsMenu.tscn")
-	print("settings")
+	
 	
 
 func Onexit_buttonbutton_down() -> void:
 	get_tree().quit()
-	print("exit")
 	
--- src/Util/ConfigFileHandler.gd
+++ src/Util/ConfigFileHandler.gd
@@ -5,9 +5,32 @@ const SETTINGS_FILE_PATH = "res://src/Util/Configfiles2.cfg"
 
 func _ready():
 	if !FileAccess.file_exists(SETTINGS_FILE_PATH):
-		config.set_value("Audio", "MainMusic", 1)
+		config.set_value("Audio", "MainMusic", 0.5)
 		config.set_value("Video", "FullScreen", false)
 		
 		config.save(SETTINGS_FILE_PATH)
 		
-		
+	else:
+		config.load(SETTINGS_FILE_PATH)
+	
+
+func _save_video_setting(key: String, value):
+	config.set_value("Video", key, value)
+	config.save(SETTINGS_FILE_PATH)
+	
+func load_video_settings():
+	var video_settings = {}
+	for key in config.get_section_keys("Video"):
+		video_settings[key] = config.get_value("Video", key)
+	return video_settings
+	
+
+func save_audio_setting(key:String, value):
+	config.set_value("Audio", key, value)
+	config.save(SETTINGS_FILE_PATH)
+	
+func load_audio_settings():
+	var audio_settings = {}
+	for key in config.get_section_keys("Audio"):
+		audio_settings[key] = config.get_value("Audio", key)
+	return audio_settings
